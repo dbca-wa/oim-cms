df4e6ed49c9040a789bbe49faa7e220b0fece643
Brian <ndwigabrian@gmail.com>
2016-04-18 14:17:18 +0800

    added metadata_link to record

--- catalogue/models.py
+++ catalogue/models.py
@@ -206,6 +206,11 @@ class Record(models.Model):
             return self.styles.get(format=format,default=True)
         except Style.DoesNotExist:
             return None
+        
+    @property
+    def metadata_link(self ):
+        return '{0}/catalogue/?version=2.0.2&service=CSW&request=GetRecordById&elementSetName=full&typenames=csw:Record&resultType=results&id={1}'.format(settings.BASE_URL,self.identifier)
+    
     
     @property
     def ows_resource(self ):
