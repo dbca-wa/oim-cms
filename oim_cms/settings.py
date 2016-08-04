072688144dc159d17d693044fd8d9b7577e20bbb
ASI <asi@dpaw.wa.gov.au>
2016-08-04 16:05:05 +0800

    Increment project version to v1.0.14.

--- oim_cms/settings.py
+++ oim_cms/settings.py
@@ -143,7 +143,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'oim_cms.urls'
 WSGI_APPLICATION = 'oim_cms.wsgi.application'
 DATABASES = {'default': database.config()}
-APPLICATION_VERSION = '1.0.13'
+APPLICATION_VERSION = '1.0.14'
 # This is required to add context variables to all templates:
 STATIC_CONTEXT_VARS = {}
 
