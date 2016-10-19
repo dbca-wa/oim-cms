d5a3cdd9912696270c4521f1e03aeeda1d78767a
Ashley Felton <ashley@ropable.com>
2016-10-19 14:35:18 +0800

    Added models to assets app (copied from IT Assets project).

--- assets/admin.py
+++ assets/admin.py
@@ -1,11 +1,17 @@
+from __future__ import unicode_literals, absolute_import
 from django.contrib.admin import register
 from reversion.admin import VersionAdmin
 
-from .models import Vendor, SoftwareLicense
+from .models import Vendor, Invoice, SoftwareLicense, HardwareModel, HardwareAsset
 
 
 @register(Vendor)
 class VendorAdmin(VersionAdmin):
+    pass
+
+
+@register(Invoice)
+class InvoiceAdmin(VersionAdmin):
     pass
 
 
@@ -15,3 +21,13 @@ class SoftwareLicenseAdmin(VersionAdmin):
     list_filter = ('oss', 'vendor')
     search_fields = ('name', 'url', 'support', 'support_url', 'vendor')
     raw_id_fields = ('org_unit', 'primary_user')
+
+
+@register(HardwareModel)
+class HardwareModelAdmin(VersionAdmin):
+    pass
+
+
+@register(HardwareAsset)
+class HardwareAssetAdmin(VersionAdmin):
+    pass
