b6cb607f620ef4f87c922f3026516779ef056da7
Ashley Felton <ashley@ropable.com>
2016-11-02 15:00:45 +0800

    Added basic unit tests for FreshdeskTicketResource.

--- oim_cms/test_api.py
+++ oim_cms/test_api.py
@@ -8,6 +8,7 @@ from uuid import uuid1
 
 from organisation.models import DepartmentUser, Location, OrgUnit, CostCentre
 from registers.models import ITSystem
+from tracking.models import FreshdeskTicket, FreshdeskContact
 
 
 def random_dpaw_email():
@@ -368,3 +369,47 @@ class ITSystemResourceTestCase(ApiTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.it1.name)
         self.assertNotContains(response, self.it2.name)
+
+
+class FreshdeskTicketResourceTestCase(ApiTestCase):
+
+    def setUp(self):
+        """Generate from FreshdeskTicket objects.
+        """
+        super(FreshdeskTicketResourceTestCase, self).setUp()
+        mixer.cycle(5).blend(
+            FreshdeskContact, email=random_dpaw_email)
+        mixer.cycle(5).blend(
+            FreshdeskTicket,
+            subject=mixer.RANDOM, description_text=mixer.RANDOM, type='Test',
+            freshdesk_requester=mixer.SELECT,
+            it_system=mixer.SELECT,
+            custom_fields={
+                'support_category': None, 'support_subcategory': None},
+        )
+        self.ticket = FreshdeskTicket.objects.first()
+
+    def test_list(self):
+        """Test the FreshdeskTicketResource list response
+        """
+        url = '/api/freshdesk_tickets/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.ticket.subject)
+
+    def test_list_filtering(self):
+        """Test the FreshdeskTicketResource filtered list response
+        """
+        self.ticket.type = 'Incident'
+        self.ticket.save()
+        url = '/api/freshdesk_tickets/?type=Test'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.ticket.subject)
+
+    def test_detail(self):
+        """Test the FreshdeskTicketResource detail response
+        """
+        url = '/api/freshdesk_tickets/{}/'.format(self.ticket.ticket_id)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
