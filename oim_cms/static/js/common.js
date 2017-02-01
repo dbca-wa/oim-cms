98e54acce82f627ae6346d95b8d1b69e4c30c3e1
Jason Moore <jason.moore@dpaw.wa.gov.au>
2017-02-01 16:28:51 +0800

    OIM Interface Improvements

--- oim_cms/static/js/common.js
+++ oim_cms/static/js/common.js
@@ -8,5 +8,25 @@ var common = {
 	getCookie: function(key) {
 		var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
 		return keyValue ? keyValue[2] : null;
+	},
+	setContentAreaHeight: function() {
+
+		var maincontentareaheight = $('#maincontentarea').height();
+		var contentareaheight= $(window).height();
+
+		if (contentareaheight > maincontentareaheight) {
+			contentareaheight = contentareaheight - 156;
+		$('#maincontentarea').css({'min-height':contentareaheight+'px'});
+		}
+
 	}
 }
+$( document ).ready(function() {
+common.setContentAreaHeight();
+common.setCookie('force_template','f6');
+
+});
+
+$(window).resize(function() {
+common.setContentAreaHeight();
+});
