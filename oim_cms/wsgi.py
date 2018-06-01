f6ceba76473e2dab578fd68c5b36bf785d8a25b6
Ashley Felton <ashley@ropable.com>
2018-06-01 13:24:09 +0800

    Reintroduce dj-static to serve media.

--- oim_cms/wsgi.py
+++ oim_cms/wsgi.py
@@ -11,7 +11,10 @@ from pathlib import Path
 d = Path(__file__).resolve().parents[1]
 dot_env = os.path.join(str(d), '.env')
 if os.path.exists(dot_env):
-    confy.read_environment_file(dot_env)
+    confy.read_environment_file(dot_env)  # Must precede dj_static import
+
+
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oim_cms.settings")
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
