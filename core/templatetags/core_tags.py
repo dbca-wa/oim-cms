2dcdfa5b1066a77a14e3d5b6d7a9cc09735faed4
Ashley Felton <ashley@ropable.com>
2018-08-21 08:26:01 +0800

    Bugfix core/templatetags/core_tags.py (preview draft error).

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -47,7 +47,9 @@ def content_list(context, value):
 def get_site_root(context):
     # NB this returns a core.Page, not the implementation-specific model used
     # so object-comparison to self will return false as objects would differ
-    return context['request'].site.root_page
+    if 'site' in context['request']:
+        return context['request'].site.root_page
+    return context
 
 
 def has_menu_children(page):
@@ -86,40 +88,52 @@ def breadcrumbs(context, calling_page):
 # a dropdown class to be applied to a parent
 @register.inclusion_tag('core/tags/f6_top_menu.html', takes_context=True)
 def f6_top_menu(context, parent, calling_page=None):
-    menuitems = parent.get_children().live().in_menu()
-    for menuitem in menuitems:
-        menuitem.show_dropdown = has_menu_children(menuitem)
-        # We don't directly check if calling_page is None since the template
-        # engine can pass an empty string to calling_page
-        # if the variable passed as calling_page does not exist.
-        menuitem.active = (calling_page.url.startswith(menuitem.url)
-                           if calling_page else False)
-    return {
-        'calling_page': calling_page,
-        'menuitems': menuitems,
-        # required by the pageurl tag that we want to use within this template
-        'request': context['request'],
-    }
+    if hasattr(parent, 'get_children'):
+        menuitems = parent.get_children().live().in_menu()
+        for menuitem in menuitems:
+            menuitem.show_dropdown = has_menu_children(menuitem)
+            # We don't directly check if calling_page is None since the template
+            # engine can pass an empty string to calling_page
+            # if the variable passed as calling_page does not exist.
+            menuitem.active = (calling_page.url.startswith(menuitem.url)
+                               if calling_page else False)
+        return {
+            'calling_page': calling_page,
+            'menuitems': menuitems,
+            # required by the pageurl tag that we want to use within this template
+            'request': context['request'],
+        }
+    else:
+        return {
+            'calling_page': calling_page,
+            'request': context['request'],
+        }
 
 # Retrieves the top menu items - the immediate children of the parent page
 # The has_menu_children method is necessary because the bootstrap menu requires
 # a dropdown class to be applied to a parent
 @register.inclusion_tag('core/tags/top_menu.html', takes_context=True)
 def top_menu(context, parent, calling_page=None):
-    menuitems = parent.get_children().live().in_menu()
-    for menuitem in menuitems:
-        menuitem.show_dropdown = has_menu_children(menuitem)
-        # We don't directly check if calling_page is None since the template
-        # engine can pass an empty string to calling_page
-        # if the variable passed as calling_page does not exist.
-        menuitem.active = (calling_page.url.startswith(menuitem.url)
-                           if calling_page else False)
-    return {
-        'calling_page': calling_page,
-        'menuitems': menuitems,
-        # required by the pageurl tag that we want to use within this template
-        'request': context['request'],
-    }
+    if hasattr(parent, 'get_children'):
+        menuitems = parent.get_children().live().in_menu()
+        for menuitem in menuitems:
+            menuitem.show_dropdown = has_menu_children(menuitem)
+            # We don't directly check if calling_page is None since the template
+            # engine can pass an empty string to calling_page
+            # if the variable passed as calling_page does not exist.
+            menuitem.active = (calling_page.url.startswith(menuitem.url)
+                               if calling_page else False)
+        return {
+            'calling_page': calling_page,
+            'menuitems': menuitems,
+            # required by the pageurl tag that we want to use within this template
+            'request': context['request'],
+        }
+    else:
+        return {
+            'calling_page': calling_page,
+            'request': context['request'],
+        }
 
 
 # Retrieves the children of the top menu items for the drop downs
