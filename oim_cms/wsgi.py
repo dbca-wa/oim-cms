4b1d17e5d22b6724b34eabc3c73d7ce694324e3e
Ashley Felton <ashley@ropable.com>
2019-07-03 15:24:31 +0800

    Factor out django-confy, update Django to 2.1.10.

--- oim_cms/wsgi.py
+++ oim_cms/wsgi.py
@@ -2,7 +2,7 @@
 WSGI config for oim_cms project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 """
-import confy
+import dotenv
 from django.core.wsgi import get_wsgi_application
 import os
 from pathlib import Path
@@ -11,7 +11,7 @@ from pathlib import Path
 d = Path(__file__).resolve().parents[1]
 dot_env = os.path.join(str(d), '.env')
 if os.path.exists(dot_env):
-    confy.read_environment_file(dot_env)  # Must precede dj_static import
+    dotenv.read_dotenv(dot_env)  # Must precede dj_static imports.
 
 
 from dj_static import Cling, MediaCling
