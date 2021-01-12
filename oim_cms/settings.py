df4770e83ef807ac305184cf8bf2e34e9c55ac41
Ashley Felton <ashley@ropable.com>
2021-01-12 10:17:15 +0800

    Update project version to 2.11.3 to match wagtail.

--- oim_cms/settings.py
+++ oim_cms/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
         },
     },
 ]
-APPLICATION_VERSION = '2.10.1'
+APPLICATION_VERSION = '2.11.3'
 # This is required to add context variables to all templates:
 STATIC_CONTEXT_VARS = {}
 
